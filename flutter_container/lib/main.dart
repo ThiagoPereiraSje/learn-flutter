3d6859d7eb6e3f5ea85c707677a5b61a3b1f2fd8
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-24 18:39:46 -0300

    Apredendo a utilizar o Container do Flutter

---
+++ flutter_container/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter Container",
+    home: Container( // Single child
+        color: Colors.white,
+        child: Column( // Multi child
+          children: [
+            Text("Texto 01"),
+            Text("Texto 02"),
+            Text("Texto 03"),
+          ],
+        ),
+    ),
+  ));
+}
